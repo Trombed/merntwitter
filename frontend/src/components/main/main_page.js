8523ec84c8b51f6bff6576535688eee72e2b737e
Eric Ho <eric.h924@gmail.com>
2020-03-15 16:46:03 -0700

    finish project

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>A Twitter Clone</h1>
+                <footer>CC 2019 Chirper</footer>
+            </div>
+        )
+    }
+}
+
+export default MainPage

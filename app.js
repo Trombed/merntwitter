30ce78dc3994fe45189a80e6a79362e18087a3e4
Eric Ho <eric.h924@gmail.com>
2020-03-15 17:37:50 -0700

    first push to heroku

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const passport = require('passport');
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 mongoose
